435dba224e31226c3804e0ac41bc9c280107b6e3
deevatanya <deewa.tatjana@yandex.ru>
2021-07-12 17:17:25 +0300

    innitial commit

---
+++ src/js/app.js
@@ -0,0 +1,47 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/style.css';
+
+import UI from './config/ui.config';
+import { validate } from './helpers/validate';
+import { showInputError, removeInputError } from './views/form';
+import { login } from './services/auth.service';
+import { notify } from './views/notifications';
+import { getNews } from './services/news.service';
+
+const { form, inputEmail, inputPassword } = UI;
+const inputs = [inputEmail, inputPassword];
+
+// Events
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  onSubmit();
+});
+inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
+
+
+// Handlers
+async function onSubmit() {
+  const isValidForm = inputs.every(el => {
+    const isValidInput = validate(el);
+    if (!isValidInput) {
+      showInputError(el);
+    }
+    return isValidInput;
+  });
+
+  if (!isValidForm) return;
+
+  try {
+    await login(inputEmail.value, inputPassword.value);
+    await getNews();
+    form.reset();
+    // show success notify
+    notify({ msg: 'Вы авторизованы успешно!', className: 'alert-success'});
+  } catch(err) {
+    // show error notify
+    notify({msg: ' Неверный логин и/или пароль!', className: 'alert-danger'});
+  }
+}
+
+// Login: denis.m.pcspace@gmail.com
+// Password: dmgame12345
---
+++ src/js/config/api.config.js
@@ -0,0 +1,4 @@
+const API_ENV = {
+  apiUrl: 'https://mlp-demo.herokuapp.com/api/public'
+};
+export default API_ENV;
